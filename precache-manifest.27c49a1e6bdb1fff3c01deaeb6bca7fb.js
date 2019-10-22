f7c546901f691d049ba63f7bf78c072e33cadd97
ozcan <28202665+ozcanseker@users.noreply.github.com>
2019-10-22 16:47:48 +0200

    Updates

---
+++ precache-manifest.27c49a1e6bdb1fff3c01deaeb6bca7fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2827ebe05e38aa2bc080650739e4f73f",
+    "url": "/namen-app/index.html"
+  },
+  {
+    "revision": "1ab8badd7f5107dc916e",
+    "url": "/namen-app/static/css/main.9807ad1e.chunk.css"
+  },
+  {
+    "revision": "f8ef9820fde31e78e055",
+    "url": "/namen-app/static/js/2.7f0473f5.chunk.js"
+  },
+  {
+    "revision": "1ab8badd7f5107dc916e",
+    "url": "/namen-app/static/js/main.9aaf008d.chunk.js"
+  },
+  {
+    "revision": "49661eababb53aec127e",
+    "url": "/namen-app/static/js/runtime-main.bf28738d.js"
+  },
+  {
+    "revision": "5d975e1bbbefb9eb55fbad23ead48aab",
+    "url": "/namen-app/static/media/Logo-kadaster.5d975e1b.png"
+  }
+]);
